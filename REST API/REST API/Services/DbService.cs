0146a9cabe14b1936b5e704b2b533aabf19c32ff
Szymon Sawicki <s24684@pjwstk.edu.pl>
2023-09-30 22:32:00 +0200

    Added students compare handler and dbservice adding student method

--- REST API/REST API/Services/DbService.cs
+++ REST API/REST API/Services/DbService.cs
@@ -1,4 +1,5 @@
 using REST_API.Models;
+using REST_API.Utils;
 
 namespace REST_API.Services;
 
@@ -6,6 +7,26 @@ public class DbService : IDbService
 {
     public void AddStudent(Student student)
     {
-        
+        var utils = new Utils.Utils();
+        var u = utils.readFromFile();
+        var students = new HashSet<Student>(new StudentCompareHandler());
+
+        foreach (var s in u)
+        {
+            students.Add(s);
+        }
+
+        if (!students.Contains(student))
+        {
+
+            if (students.Add(student))
+            {
+                students.Add(student);
+                    
+            }
+        }
+
+
+        utils.SaveCollection(students);
     }
 }
--- REST API/REST API/Utils/StudentCompareHandler.cs
+++ REST API/REST API/Utils/StudentCompareHandler.cs
@@ -1,6 +1,18 @@
+using System.Diagnostics.CodeAnalysis;
+using REST_API.Models;
+
 namespace REST_API.Utils;
 
-public class StudentCompareHandler
+public class StudentCompareHandler : IEqualityComparer<Student>
 {
-    
+    public bool Equals(Student x, Student y)
+    {
+           
+        return StringComparer.CurrentCultureIgnoreCase.Equals($"{x.IdStudent}", $"{y.IdStudent}");
+    }
+
+    public int GetHashCode([DisallowNull] Student student)
+    {
+        return StringComparer.CurrentCultureIgnoreCase.GetHashCode($"{student.IdStudent}");
+    }
 }
